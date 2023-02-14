f08d47d38b2ab04e67f4d08d1a43be1aa585badc
Ajibola <davidajibolatimilehin@gmail.com>
2023-02-14 09:57:28 +0100

    new commit

--- lecture_01.py
+++ lecture_01.py
@@ -40,8 +40,39 @@ class LinkedList:
         if self.length == 0:
             self.head = None 
             self.tail = None 
-        return temp 
+        return temp
+
+    def unshift(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return self
+
+        new_node.next = self.head 
+        self.head = new_node
+
+        return self
     
+    def shift(self):
+
+        if self.head is None:
+            print("The linked list is empty")
+            return 
+        
+        temp = self.head 
+        self.head = self.head.next
+        self.length -= 1 
+        
+        if not self.head: 
+            self.head = None 
+            self.tail = None 
+            print("No more element")
+
+        temp.next = None
+        return self
+            
     def __repr__(self):
         current_node = self.head
         linked = ""
@@ -64,4 +95,10 @@ linked.insert_at_end(3)
 for i in range(4, 11):
     linked.insert_at_end(i)
 linked.remove_at_end()
+linked.unshift(10)
+linked.shift()
+
+new = LinkedList(1)
+new.shift()
 print(linked)
+print(new)
